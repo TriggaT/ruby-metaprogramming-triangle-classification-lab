9d2dac8fb97fca7f1eb786a9bbcfae1ff154c945
Trayshawn <traytwebb14@yahoo.com>
2020-11-26 16:13:21 -0600

    commit tree b96c62bb1cfaf06e4893822bf4b781b1c0df531b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require "pry"
+
+
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+   
+  end 
+
+  def kind
+    triangle? 
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else :scalene 
+    end
+  end
+
+
+
+  def triangle?
+    tri = [(a + b > c), (b + c > a),(a + c > b )]
+    tri.include?(false) || tri.include?(0)
+    raise TriangleError if tri.include?(false)
+  end 
+    
+
+
+  
+  class TriangleError < StandardError
+  end
+
+
+
 end
